4a228a5b94c00fd50b4be95a9d3202f765612b1a
aco-rt <grpnewaa@gmail.com>
2025-04-28 14:05:52 -0600

    folders

---
+++ src/content/sop/admin-fun/_meta.tsx
@@ -0,0 +1,11 @@
+export default {
+    "verbal-communication": "",
+    "radio-communications": "",
+    "nonverbal-communications": "",
+    "ace-report": "",
+    "ddd": "",
+    "locstat-sitrep": "",
+    "indirect-fire-support": "",
+    "common-phrases": "",
+}
+  
