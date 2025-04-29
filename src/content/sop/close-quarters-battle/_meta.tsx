813f598c9fc44a70e403a63daa4e257c088339dc
aco-rt <48486853+aco-rt@users.noreply.github.com>
2025-04-28 18:08:43 -0600

    Aco changes (#9)

---
+++ src/content/sop/close-quarters-battle/_meta.tsx
@@ -0,0 +1,8 @@
+export default {
+    "before-entry": "",
+    "clearing-methods": "",
+    "prepping-a-room": "",
+    "flowing-into-a-room": "",
+    "room-anatomy": "",
+    "threat-priorities-and-communication": "",
+}
